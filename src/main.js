408fbf4ecad95353878190722ac5d8ad78450f65
Mikey Kriegel <mikkrieg@gmail.com>
2021-02-10 10:30:55 -0800

    Add click functionality to buttons
    Co-authored-by: Jonah Johansen <johansenjonah+git@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,4 +1,43 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import {SpaceX} from './js/getspacex';
+
+
+function main(){
+  $('#infoBox').hide();
+  $('#latestLaunch').on("click", function(){
+    $('#selectBox').hide();
+    $('#infoBox').show();
+  });
+  $('#upcomingLaunch').on("click", function(){
+    $('#selectBox').hide();
+    $('#infoBox').show();
+  });
+  $('#pastLaunch').on("click", function(){
+    $('#selectBox').hide();
+    $('#infoBox').show();
+  });
+  $('#rockets').on("click", function(){
+    $('#selectBox').hide();
+    $('#infoBox').show();
+  });
+  $('#sat').on("click", function(){
+    $('#selectBox').hide();
+    $('#infoBox').show();
+  });
+  $('#about').on("click", function(){
+    $('#selectBox').hide();
+    $('#infoBox').show();
+  });
+  $('#hist').on("click", function(){
+    $('#selectBox').hide();
+    $('#infoBox').show();
+  });
+  //Grab user inputs
+  // pass to the api
+  console.log(SpaceX.getSpaceX());
+}
+
+main();
